51fc5c586f0b42c838e3e0639abedebaea49d818
rational-dispatch-0384 <baja903@gmail.com>
2019-04-21 15:55:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
-functions = destructivelyAppendKitten("Bob");
+function destructivelyAppendKitten("Milo", "Otis"){
+  
+}
