578849efa05704688b693a4d0ec02888f42fd52f
rational-dispatch-0384 <baja903@gmail.com>
2019-04-21 16:01:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
 
+function appendKitten(name){
+  var newArray = kittens.slice();
+  // or ES6 way
+  // var newArray = [...kittens];
+  newArray.push(name)
+  return newArray
